07cf656c17a9df103cb1f359747d44582a772e64
chikealvan <nwadoziechikealvan@gmail.com>
2023-08-27 12:06:48 +0100

    added arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply = 31 * 2;
+console.log(multiply);
+
+let random = 0;
+console.log(random);
+
+let mod = 24 / 5;
+console.log(mod);
+
+
+let max = (19, 10, 5, 2, 11, 20);
+console.log(Math.max(11, 19, 20));
